39ebc092020311421d3f9691c14329c95556018b
Daniil Stepanov <kos@slicetech.ru>
2021-10-11 18:27:20 +0300

    Search mechanism and json array builder fixed #1

--- src/http/handlers/Handler.java
+++ src/http/handlers/Handler.java
@@ -2,18 +2,18 @@ package http.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import http.json.JsonArrayBuilder;
+import notes.Notes;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class Handler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "It works!";
-        exchange.sendResponseHeaders(200, response.length());
-        OutputStream outputStream = exchange.getResponseBody();
-        outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-        outputStream.close();
+        String response = new JsonArrayBuilder().buildAllNotes(new Notes().getNotesList());
+        exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
+        exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
+        exchange.close();
     }
 }
--- src/http/json/JsonArrayBuilder.java
+++ src/http/json/JsonArrayBuilder.java
@@ -1,16 +1,38 @@
 package http.json;
 
 import notes.Note;
+import notes.Notes;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonArrayBuilder {
-    private final Note passNote = new Note("Пароли", "А их тут нет", LocalDateTime.now());
 
-    public String build() {
-        return "{" + "\"notes" + "\"" + ": " +
-                "[ " + new JsonBuilder().build(passNote) +
-                " ]" +
-                "}";
+    public String buildAllNotes(List<Note> notes) {
+        StringBuilder result = new StringBuilder("[");
+        List<String> notesString = new ArrayList<>();
+        for (Note note : notes) {
+            String noteString = new JsonBuilder().build(note);
+            notesString.add(noteString);
+        }
+        String collectedArray = String.join(", ", notesString);
+        result.append(collectedArray);
+        result.append("]");
+        return result.toString();
+    }
+
+    public String foundedNotes(List<Note> notes) {
+        StringBuilder result = new StringBuilder("[");
+        List<String> notesString = new ArrayList<>();
+        for (Note note : notes) {
+            if (note.getTitle().toLowerCase().contains("пароли")) {
+                String noteString = new JsonBuilder().build(note);
+                notesString.add(noteString);
+            }
+        }
+        String collectedList = String.join(", ", notesString);
+        result.append(collectedList);
+        result.append("]");
+        return result.toString();
     }
 }
